47fc0bcdba9c257193bc99484c80d6e3b724428d
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-02-28 17:03:19 -0500

    Add Login and Register

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthHomeComponent } from './auth-home.component';
+import { LoginComponent } from './ninos/login/login.component';
+import { RegisterComponent } from './ninos/register/register.component';
 
 const routes: Routes = [
   {
@@ -10,6 +12,14 @@ const routes: Routes = [
       {
         path: '',
         component: AuthHomeComponent
+      },
+      {
+        path: 'ninos/login',
+        component: LoginComponent
+      },
+      {
+        path: 'ninos/register',
+        component: RegisterComponent
       }
     ]
   }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthHomeComponent } from './auth-home.component';
+import { LoginComponent } from './ninos/login/login.component';
+import { RegisterComponent } from './ninos/register/register.component';
 
 
 @NgModule({
   declarations: [
-    AuthHomeComponent
+    AuthHomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
