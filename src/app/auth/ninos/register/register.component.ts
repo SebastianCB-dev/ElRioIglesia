9b6304e7c3afbfde5cb5bc796bb3c52c9338acc6
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-01 09:51:50 -0500

    Added Form Validators

--- src/app/auth/ninos/register/register.component.ts
+++ src/app/auth/ninos/register/register.component.ts
@@ -11,19 +11,39 @@ export class RegisterComponent {
   public registerForm: FormGroup = this.fb.group({
     "email": ['', [Validators.required, Validators.email]],
     "documento": ['', [Validators.required, Validators.min(1)]],
-    "fullname": ['', [Validators.required]],
+    "fullname_nino": ['', [Validators.required]],
     "fullname_acudiente": ['', [Validators.required]],
     "dob": ['', [Validators.required]],
+    "terms": [false, [Validators.requiredTrue]]
+  },
+  {
+    validators: this.checkDate()
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   isValidControl(control: string) {
     return this.registerForm.get(control)?.touched &&
            this.registerForm.get(control)?.invalid;
   }
 
-  register() {
+  register() {    
+    if(this.registerForm.invalid) {
+      return this.registerForm.markAllAsTouched();
+    }
+    console.log('Registering');
+  }
 
+  getDateToday() {
+    return new Date().toISOString().split('T')[0];
+  }
+
+  checkDate() {
+    return (formGroup: FormGroup) => {
+      if(formGroup.get('dob')?.value > this.getDateToday()) {
+        formGroup.get('dob')?.setErrors({invalidDate: true});
+      }
+      return null;
+    }
   }
 }
