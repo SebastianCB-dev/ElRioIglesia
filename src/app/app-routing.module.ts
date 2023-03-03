d88e4354a37d5bc29204301cb5f6fd7855147e47
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-03 09:24:39 -0500

    Added Ninos Profile route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,10 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
+    path: 'ninos',
+    loadChildren: () => import('./ninos/ninos.module').then((m) => m.NinosModule)
   }
 ];
 
