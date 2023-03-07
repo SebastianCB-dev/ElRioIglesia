d079b7180a37c8c44756ce576f72490b72b9e1cd
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-07 11:26:24 -0500

    Add Admin Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard as AuthAdminGuard } from './guards/auth-admin.guard';
 
 import { AuthGuard as AuthNinosGuard } from './guards/auth-ninos.guard';
 
@@ -24,6 +25,8 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
+    canActivate: [AuthAdminGuard],
+    canLoad: [AuthAdminGuard],
   }
 ];
 
---
+++ src/app/guards/auth-admin.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Route, RouterStateSnapshot, UrlSegment, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+
+  constructor(private userSrv: UserService,
+              private router: Router) {}
+
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    await this.userSrv.loadUser();
+    if (!this.userSrv.getUser() || 
+         this.userSrv.getUser()?.role !== 'admin') {
+      await this.router.navigateByUrl('/auth');
+    } 
+    return true;
+  }
+
+  async canLoad(route: Route, segments: UrlSegment[]) {
+    await this.userSrv.loadUser();
+    if (!this.userSrv.getUser() 
+        || this.userSrv.getUser()?.role !== 'admin') {
+      await this.router.navigateByUrl('/auth');
+      return false;
+    }
+    return true;
+  }
+}
