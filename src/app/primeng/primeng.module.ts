09ceefefdb1d25ebca8c145ad5ddbf8c2708c470
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-07 18:53:35 -0500

    Import TableModule

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   exports: [
+    TableModule,
     ToastModule,
     ProgressSpinnerModule
   ],
