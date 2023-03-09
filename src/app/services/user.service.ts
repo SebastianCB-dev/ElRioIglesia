a2dbe664fa64edd3f103eeb55e1d26e3278fd772
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2023-03-09 18:38:28 -0500

    Add Crypto.js encrypt and decrypt method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interface/user';
+
+import * as CryptoJS from 'crypto-js';
+
 import { FirebaseService } from './firebase.service';
+import { PRIVATE_KEY } from '../constants/constants.example';
+
 
 @Injectable({
   providedIn: 'root'
@@ -24,8 +29,8 @@ export class UserService {
   async loadUser() {
     if(this._user)
       return;
-    const token = localStorage.getItem('token-rio') || '';
-
+    const dataToken = localStorage.getItem('token-rio') || '';
+    const token = this.decryptData(dataToken);
     if(token) {
       const user = await this.fbSrv.getUserByID(Number(token));
       if(user) {
@@ -41,6 +46,10 @@ export class UserService {
   }
 
   cryptData(data: string) {
-    return 
+    return CryptoJS.AES.encrypt(data, PRIVATE_KEY).toString();
+  }
+
+  decryptData(data: string) {
+    return CryptoJS.AES.decrypt(data, PRIVATE_KEY).toString(CryptoJS.enc.Utf8);
   }
 }
